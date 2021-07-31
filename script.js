15534da63d0e3b157aa031de9fc9fc914641f655
Arina Gubina <fike-01@mail.ru>
2021-07-31 20:58:24 +0700

    Урок 2

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const money = 0;
-let income = 1;
-let addExpenses = 2;
-let deposit = [];
-let mission; 
-let period = true;
-alert('Hello');
-console.log('world');
+const money = 40000;
+let income = 'репетиторство';
+let addExpenses = 'Тест-полоски,Инсулин,Айпорты,Мониторинг';
+let deposit = true;
+let mission = 1000000; 
+let period = 12;
+
+console.log( typeof(money) );
+console.log( typeof(income) );
+console.log( typeof(deposit) );
+
+console.log( typeof(addExpenses.length) );
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLocaleLowerCase();
+addExpenses = addExpenses.split(',');
+for (let i = 0; i < addExpenses.length; i++) {
+    console.log(addExpenses[i]);
+}
+
+let budgetDay = money/30;
+console.log(budgetDay);
