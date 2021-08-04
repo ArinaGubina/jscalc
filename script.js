5c59e489fee24217db2fd187a145fa6f068c0fb2
Arina Gubina <fike-01@mail.ru>
2021-08-04 18:27:51 +0700

    Урок 4

--- script.js
+++ script.js
@@ -13,10 +13,6 @@ let amount2 = +prompt('Во сколько это обойдется?', 0);
 let mission = 1000000; 
 let period = 12;
 
-console.log( typeof(money) );
-console.log( typeof(income) );
-console.log( typeof(deposit) );
-
 console.log(addExpenses.length);
 
 console.log('Период равен ' + period + ' месяцев');
@@ -28,21 +24,46 @@ for (let i = 0; i < addExpenses.length; i++) {
     console.log(addExpenses[i]);
 }
 
-let budgetMonth = money - amount1 - amount2;
-console.log('Бюджет на месяц: ' + budgetMonth);
+let showTypeOf = function(data) {
+    console.log(data, typeof(data) );
+};
 
-let budgetDay = Math.floor(budgetMonth/30);
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+function getExpensesMonth() {
+    return amount1 + amount2;
+}
+
+function getAccumulatedMonth() {
+    return money - getExpensesMonth();
+}
+
+let accumulatedMonth = getAccumulatedMonth();
+
+function getTargetMonth() {
+    return Math.ceil(mission/accumulatedMonth);
+}
+
+console.log('Бюджет на месяц: ' + accumulatedMonth);
+
+let budgetDay = Math.floor(accumulatedMonth / 30);
 console.log('Бюджет на день: ' + budgetDay);
 
-let realPeriod = Math.ceil(mission/budgetMonth);
+let realPeriod = Math.ceil(mission / accumulatedMonth);
 console.log('Цель будет достигнута за ' + realPeriod + ' месяцев');
 
-if (budgetDay > 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay > 600) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay > 0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что то пошло не так');
-}
+let getStatusIncome = function() {
+    if (budgetDay > 1200) {
+        return 'У вас высокий уровень дохода';
+    } else if (budgetDay > 600) {
+        return 'У вас средний уровень дохода';
+    } else if (budgetDay > 0) {
+        return 'К сожалению у вас уровень дохода ниже среднего';
+    } else {
+        return 'Что то пошло не так';
+    }
+};
+
+console.log( getStatusIncome() );
